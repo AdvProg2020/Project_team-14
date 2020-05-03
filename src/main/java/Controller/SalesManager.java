4074720e7053b89ea08184ceebbc3a6991893e5b
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-05-03 20:54:03 +0430

    updated SalesManager

--- src/main/java/Controller/SalesManager.java
+++ src/main/java/Controller/SalesManager.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import Model.Off.Sale;
+import Model.Request.ChangeSaleRequest;
+import Model.Request.Request;
 import Model.Storage;
 
 import java.text.ParseException;
@@ -8,13 +10,13 @@ import java.util.ArrayList;
 
 public class SalesManager {
 
-    public void listSales (String sortFactor) {
+    public void listSales (String salesmanID, String sortFactor) {
         StringBuilder ans = new StringBuilder();
-        //ArrayList<Sale> allSales = Storage.getAllSales();
+        ArrayList<Sale> allSales = Sale.getAllSaleBySalesmanID(salesmanID);
         /*
          * what are the sort factor??
          */
-        for (Sale sale : Storage.allSales) {
+        for (Sale sale : allSales) {
             ans.append(sale.toString());
         }
         Server.setAnswer(ans.toString());
@@ -33,25 +35,13 @@ public class SalesManager {
 
     public void editSale (String saleID, String attribute, String updatedInfo) {
         String ans = "your request to update Sale info has been sent to manager";
-        Sale selectedSale = Sale.getSaleByID(saleID);
-        if (attribute.equals("startDate")) {
-           // new Request("editSale", saleID, attribute, updatedInfo);
-        } else if (attribute.equals("endDate")) {
-         //   new Request("editSale", saleID, attribute,updatedInfo);
-        } else if (attribute.equals("addProduct")) {
-         //   new Request("editSale", saleID, attribute, updatedInfo);
-        } else if (attribute.equals("removeProduct")) {
-          //  new Request("editSale", saleID, attribute, updatedInfo);
-        } else if (attribute.equals("percentage")) {
-         //   new Request("editSale", saleID, attribute, updatedInfo);
-        } else {
-            ans = "sales do not have property like want you want";
-        }
+        Sale sale = Sale.getSaleByID(saleID);
+        new ChangeSaleRequest(sale.getSalesmanID(), sale, attribute, updatedInfo);
         Server.setAnswer(ans);
     }
 
     public void addSale (ArrayList<String> info) throws ParseException {
         Sale newSale = new Sale(info.get(0), info.get(1), Integer.parseInt(info.get(2)), info.get(3));
-      //  new Request("addSale", newSale.getSaleID());
+        new Request(info.get(3), newSale, "ADD_NEW_SALE");
     }
 }
--- src/main/java/Controller/SalesmanManager.java
+++ src/main/java/Controller/SalesmanManager.java
@@ -5,18 +5,19 @@ import Model.Account.Salesman;
 import Model.Storage;
 
 public class SalesmanManager {
+
     public void register(String[] information) {
         if (Storage.isThereAccountWithUsername(information[3])) {
             Server.setAnswer("username has already been taken");
         }
         Server.setAnswer("register successful");
         new Salesman(information[3], information[4], information[1], information[2],
-                information[6], information[7], information[5], information[8], 0);
+                information[6], information[7], information[5], information[8]);
     }
 
     public void showCompanyInfo (String salesmanID) {
         Salesman salesman = (Salesman) Storage.getAccountWithUsername(salesmanID);
-        Server.setAnswer(salesman.getCompany());
+        Server.setAnswer("Salesman Company Info : " + salesman.getCompany());
     }
 
     public void showSalesHistory (String salesmanID) {
