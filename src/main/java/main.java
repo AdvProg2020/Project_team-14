9bb82234157fb801a5885ce8cb639a6285da577f
MartinBrave <matin.shoja23@gmail.com>
2020-06-12 19:55:13 +0430

    make comment

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,17 +5,34 @@ import java.util.Scanner;
 import Controller.Server;
 import Menus.MainMenu;
 import Menus.Menu;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class main {
+public class main extends Application {
+
 
     public static void main(String[] args) throws ParseException, IOException, ClassNotFoundException {
-
         Server server = new Server();
         Menu.setServer(server);
-        Menu.setScanner(new Scanner(System.in));
-        MainMenu mainMenu = new MainMenu(null, "Main Menu");
-        mainMenu.execute();
+        //Menu.setScanner(new Scanner(System.in));
+        //MainMenu mainMenu = new MainMenu(null, "Main Menu");
+        launch(args);
+        //mainMenu.execute();
     }
 
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/RegisterBoss/RegisterBoss.fxml"));
+        stage.setScene(new Scene(root, 1920, 1080));
+        stage.setTitle("RegisterForBossMenu");
+        stage.setResizable(false);
+        //stage.getIcons().add(new Image(getClass().getResourceAsStream("Pictures/logo.jpg")));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
 }
