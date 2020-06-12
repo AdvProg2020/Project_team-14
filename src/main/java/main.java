6ef321c06d4ab48df3b7f36869a646cf4cab8a89
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-06-12 19:53:35 +0430

    nothing ;)

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,11 +5,24 @@ import java.util.Scanner;
 import Controller.Server;
 import Menus.MainMenu;
 import Menus.Menu;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class main {
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/Login/Login.fxml"));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) throws ParseException, IOException, ClassNotFoundException {
-
+        launch(args);
         Server server = new Server();
         Menu.setServer(server);
         Menu.setScanner(new Scanner(System.in));
