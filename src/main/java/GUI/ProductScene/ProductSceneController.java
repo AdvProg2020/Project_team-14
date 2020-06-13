eb63bb7863a122a28ffc37f58764604136a58d27
MartinBrave <matin.shoja23@gmail.com>
2020-06-13 14:41:10 +0430

    GUI

--- src/main/java/GUI/ProductScene/ProductSceneController.java
+++ src/main/java/GUI/ProductScene/ProductSceneController.java
@@ -1,14 +1,24 @@
 package GUI.ProductScene;
 
+import com.google.common.eventbus.DeadEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ProductSceneController {
 
     public ImageView accountMenuButton;
 
-    public void openPopUp(MouseEvent mouseEvent) {
-
+    public void openPopUp(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/Register/Register.fxml"));
+        Stage stage = (Stage) ((Button) mouseEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
     }
 
     public void initialize() {
