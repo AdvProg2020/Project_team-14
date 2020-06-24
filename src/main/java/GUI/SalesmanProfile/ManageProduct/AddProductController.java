b0db9138f76bc704f7943c40149563c7a01165e5
MartinBrave <matin.shoja23@gmail.com>
2020-06-25 02:36:29 +0430

    add new product

--- src/main/java/GUI/SalesmanProfile/ManageProduct/AddProductController.java
+++ src/main/java/GUI/SalesmanProfile/ManageProduct/AddProductController.java
@@ -1,5 +1,6 @@
 package GUI.SalesmanProfile.ManageProduct;
 
+import GUI.MenuHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,6 +15,9 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddProductController {
     public ImageView img;
@@ -41,13 +45,87 @@ public class AddProductController {
         path = String.valueOf(chosenFile.toURI());
     }
 
-    public void newProduct(ActionEvent actionEvent) {
+    private void reset(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/SalesmanProfile/ManageProduct/NewProductLayout.fxml"));
+        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
 
+    public void newProduct(ActionEvent actionEvent) throws IOException, ParseException {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "", ButtonType.OK);
+        if (productName.getText().equalsIgnoreCase("")) {
+            alert.setContentText("You Should Choose A Name For Product");
+            alert.showAndWait();
+            reset(actionEvent);
+        } else if (brand.equals("")) {
+            alert.setContentText("You Should Choose A Brand For Product");
+            alert.showAndWait();
+            reset(actionEvent);
+        } else if (description.equals("")) {
+            alert.setContentText("You Should Write A Description For Product");
+            alert.showAndWait();
+            reset(actionEvent);
+        } else if (price.equals("")) {
+            alert.setContentText("You Should Choose A Price For Product");
+            alert.showAndWait();
+            reset(actionEvent);
+        } else {
+            if (!checkProductNameFormat(productName.getText())) {
+                alert.setContentText("Product Name Format Is Invalid");
+                alert.showAndWait();
+                reset(actionEvent);
+            }
+            if (!checkNameFormat(brand.getText())) {
+                alert.setContentText("Product Brand Format Is Invalid");
+                alert.showAndWait();
+                reset(actionEvent);
+            }
+            if (!price.getText().matches("\\d+") || ((String) price.getText()).length() > 8) {
+                alert.setContentText("You Should Choose A Price");
+                alert.showAndWait();
+                reset(actionEvent);
+            }
+            if (!checkDescriptionFormat(description.getText())) {
+                alert.setContentText("Description Format Is Invalid");
+                alert.showAndWait();
+                reset(actionEvent);
+            }
+            MenuHandler.getServer().clientToServer("create product+" + MenuHandler.getUsername() + "+" +
+                    productName.getText() + "+" + brand.getText() + "+" + description.getText() + "+" + price.getText()
+                    + "+" + count.getValue());
+            String serverAnswer = MenuHandler.getServer().serverToClient();
+            System.out.println(serverAnswer);
+            if (serverAnswer.equals("product created")) {
+                alert.setAlertType(Alert.AlertType.INFORMATION);
+                alert.setContentText("Product Created");
+            } else {
+                alert.setContentText(serverAnswer);
+            }
+            alert.showAndWait();
+            reset(actionEvent);
+        }
     }
 
     public void back(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/GUI/SalesmanProfile/SalesmanProfileLayout.fxml"));
         Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
+    }
+
+    private boolean checkProductNameFormat(String input) {
+        return getMatcher("([\\d\\w_\\-,\\s])+", input).matches();
+    }
+
+    private boolean checkDescriptionFormat(String description) {
+        return getMatcher("[\\w\\s\\.]+", description).matches();
+    }
+
+    private boolean checkNameFormat(String name) {
+        return getMatcher("[a-zA-Z]+", name).matches();
+    }
+
+    private Matcher getMatcher(String regex, String command) {
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(command);
     }
 }
