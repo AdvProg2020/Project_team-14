3e1662c34431e4590be9e9eb420c6b094c20ad1f
h-hafezi <h.hafezi2000@gmail.com>
2020-06-25 05:26:05 +0430

    adding saleMenu and Cart and Category menu

--- src/main/java/GUI/CategoryMenu/SingleCategory.java
+++ src/main/java/GUI/CategoryMenu/SingleCategory.java
@@ -1,25 +1,41 @@
 package GUI.CategoryMenu;
 
 import GUI.MenuHandler;
+import Menus.Menu;
+import Menus.Views.ViewCategoryMenu;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 public class SingleCategory {
     public Label parentCategory;
     public Label categoryName;
     public Label attribute;
 
-    public void ChangeNameClicked(MouseEvent mouseEvent) {
+    public void ChangeNameClicked(MouseEvent mouseEvent) throws ParseException, IOException {
+        TextInputDialog td = new TextInputDialog("enter the new name without space");
+        td.setHeaderText("type here ...");
+        td.showAndWait();
+        String newName = td.getEditor().getText();
+        if (!newName.contains(" ")) {
+            MenuHandler.getServer().clientToServer("edit category name+" + MenuHandler.getUsername() + "+" +
+                    Category.selected_category.getCategoryName() + "+" + newName);
+            String answer = MenuHandler.getServer().serverToClient();
+            if (answer.equals("edit successful")) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Successful", ButtonType.OK);
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Unsuccessful", ButtonType.OK);
+                alert.showAndWait();
+            }
+        }
 
     }
 
@@ -39,7 +55,7 @@ public class SingleCategory {
         stage.setScene(new Scene(root));
     }
 
-    public void initialize(){
+    public void initialize() {
         parentCategory.setText(Category.selected_category.getParentCategoryName());
         categoryName.setText(Category.selected_category.getCategoryName());
         attribute.setText(Category.selected_category.getAttribute());
