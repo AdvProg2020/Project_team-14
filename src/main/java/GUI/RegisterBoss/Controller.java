ed604843e7b6d6e1a417561a0acfc1a98c50e0a7
h-hafezi <h.hafezi2000@gmail.com>
2020-06-26 01:49:13 +0430

    register boss

--- src/main/java/GUI/RegisterBoss/Controller.java
+++ src/main/java/GUI/RegisterBoss/Controller.java
@@ -1,4 +1,4 @@
-package GUI.Register;
+package GUI.RegisterBoss;
 
 import GUI.MenuHandler;
 import javafx.collections.FXCollections;
@@ -22,13 +22,7 @@ import java.text.ParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegisterController {
-
-    public Label companyLabel;
-    public MediaView mediaView;
-    ObservableList<String> rolesIfHasNotBoss = FXCollections.observableArrayList("Boss", "Salesman", "Customer");
-    ObservableList<String> rolesIfHasBoss = FXCollections.observableArrayList("Customer", "Salesman");
-
+public class Controller {
     public TextField firstName;
     public TextField lastName;
     public PasswordField password;
@@ -36,29 +30,10 @@ public class RegisterController {
     public TextField Email;
     public ChoiceBox role;
     public TextField telephone;
-    public TextField company;
+    public MediaView mediaView;
+    public MediaView film;
+    ObservableList<String> rolesIfHasBoss = FXCollections.observableArrayList("Boss");
 
-    public void initialize() throws ParseException, IOException {
-        String path = "src/main/java/GUI/Login/resources/mp4.mp4";
-        Media media = new Media(new File(path).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaView.setMediaPlayer(mediaPlayer);
-        mediaPlayer.setAutoPlay(true);
-        mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
-
-        MenuHandler.getServer().clientToServer("is server has boss");
-        if (MenuHandler.getServer().serverToClient().equalsIgnoreCase("yes")) {
-            role.setItems(rolesIfHasBoss);
-        } else {
-            role.setItems(rolesIfHasNotBoss);
-        }
-    }
-
-    public void openLoginMenu(ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/GUI/Login/Login.fxml"));
-        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-    }
 
     public void register(ActionEvent actionEvent) throws ParseException, IOException {
         Alert alert = new Alert(Alert.AlertType.ERROR, "", ButtonType.OK);
@@ -83,20 +58,17 @@ public class RegisterController {
         } else if (telephone.getText().equals("")) {
             alert.setContentText("Telephone Field Must Not Be Empty");
             alert.showAndWait();
-        } else if (((String) role.getValue()).equalsIgnoreCase("salesman") && company.getText().equals("")) {
-            alert.setContentText("Company Field Must Not Be Empty");
-            alert.showAndWait();
         } else {
-            if (!checkUsernameFormat(username.getText())) {
+            if (checkUsernameFormat(username.getText())) {
                 alert.setContentText("Username Should Consists Of Only Characters And Numbers And _ Without Space");
                 alert.showAndWait();
-            } else if (!checkUsernameFormat(password.getText())) {
+            } else if (checkUsernameFormat(password.getText())) {
                 alert.setContentText("Password Should Consists Of Only Characters And Numbers And _ Without Space");
                 alert.showAndWait();
-            } else if (!checkNameFormat(firstName.getText())) {
+            } else if (checkNameFormat(firstName.getText())) {
                 alert.setContentText("Name Should Consists Of Only Characters Like [A-Z][a-z]");
                 alert.showAndWait();
-            } else if (!checkNameFormat(lastName.getText())) {
+            } else if (checkNameFormat(lastName.getText())) {
                 alert.setContentText("Name Should Consists Of Only Characters Like [A-Z][a-z]");
                 alert.showAndWait();
             } else if (!checkEmailFormat(Email.getText())) {
@@ -109,34 +81,15 @@ public class RegisterController {
                 String message = "register+" + firstName.getText() + "+" + lastName.getText() + "+" + username.getText()
                         + "+" + password.getText() + "+" + ((String) role.getValue()) + "+" + Email.getText()
                         + "+" + telephone.getText();
-                if (((String) role.getValue()).equalsIgnoreCase("salesman")) {
-                    message += "+" + company.getText();
-                }
                 (MenuHandler.getServer()).clientToServer(message);
-                String serverAnswer;
-                serverAnswer = (MenuHandler.getServer()).serverToClient();
-                if (serverAnswer.equalsIgnoreCase("the username is already taken, try something else")) {
-                    alert.setContentText("This Username Has Been Taken Please Try Something Else");
-                } else {
-                    alert.setAlertType(Alert.AlertType.CONFIRMATION);
-                    alert.setContentText("Your Register Was Successful");
-                }
+                (MenuHandler.getServer()).serverToClient();
+                alert.setAlertType(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("Your Register Was Successful");
                 alert.showAndWait();
-                Parent root = FXMLLoader.load(getClass().getResource("/GUI/Register/Register.fxml"));
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/ProductScene/ProductScene.fxml"));
                 Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
                 stage.setScene(new Scene(root));
             }
-        }
-    }
-
-    public void checkSalesmanRole() {
-        if (role.getValue() == null) return;
-        if (((String) role.getValue()).equalsIgnoreCase("salesman")) {
-            companyLabel.setVisible(true);
-            company.setVisible(true);
-        } else {
-            company.setVisible(false);
-            companyLabel.setVisible(false);
         }
     }
 
@@ -146,11 +99,11 @@ public class RegisterController {
     }
 
     private boolean checkNameFormat(String name) {
-        return getMatcher("[a-zA-Z]+", name).matches();
+        return !getMatcher("[a-zA-Z]+", name).matches();
     }
 
     private boolean checkUsernameFormat(String input) {
-        return getMatcher("(\\w+)", input).matches();
+        return !getMatcher("(\\w+)", input).matches();
     }
 
     private boolean checkEmailFormat(String mail) {
@@ -174,5 +127,25 @@ public class RegisterController {
         if (alert.getResult().equals(ButtonType.YES)) {
             System.exit(23);
         }
+    }
+
+    public void waterfall() {
+        String path = "src/main/java/GUI/Register/resources/mp4.mp4";
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        film.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+    }
+
+    public void initialize() {
+        role.setItems(rolesIfHasBoss);
+        String path = "src/main/java/GUI/Login/resources/mp4.mp4";
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+        waterfall();
     }
 }
