804384108136a862828cc84de3249e95905402d4
h-hafezi <h.hafezi2000@gmail.com>
2020-07-09 07:16:38 +0430

    updating security

--- src/main/java/Controller/Security/Methods.java
+++ src/main/java/Controller/Security/Methods.java
@@ -1,5 +1,8 @@
 package Controller.Security;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class Methods {
     public static boolean checkStringLength(String command) {
         return command.length() >= 10000;
@@ -8,5 +11,31 @@ public class Methods {
     public static boolean mayContainScript(String command) {
         return command.contains("<") || command.contains(">") || command.contains("\\") || command.contains("/");
     }
+
+    public String encode(String string) {
+        String s1 = Base64.getEncoder().encodeToString(string.getBytes());
+        String s2 = Base64.getEncoder().encodeToString(s1.getBytes());
+        String s3 = Base64.getEncoder().encodeToString(s2.getBytes());
+        String s4 = Base64.getEncoder().encodeToString(s3.getBytes());
+        return Base64.getEncoder().encodeToString(s4.getBytes());
+    }
+
+    public String decode(String string) {
+        String s4 = new String(Base64.getDecoder().decode(string));
+        String s3 = new String(Base64.getDecoder().decode(s4));
+        String s2 = new String(Base64.getDecoder().decode(s3));
+        String s1 = new String(Base64.getDecoder().decode(s2));
+        return new String(Base64.getDecoder().decode(s1));
+    }
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
+
+    public static String generateNewToken() {
+        byte[] randomBytes = new byte[1000];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+    }
+
 
 }
--- src/main/java/Model/RandomString.java
+++ src/main/java/Model/RandomString.java
@@ -1,9 +1,5 @@
 package Model;
 
-import java.io.Serializable;
-import java.security.SecureRandom;
-import java.util.Base64;
-
 public abstract class RandomString {
     public static String getRandomString(int n) {
         String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -27,15 +23,6 @@ public abstract class RandomString {
         result += "---";
         result += getRandomString(23);
         return result;
-    }
-
-    private static final SecureRandom secureRandom = new SecureRandom();
-    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
-
-    public static String generateNewToken() {
-        byte[] randomBytes = new byte[1000];
-        secureRandom.nextBytes(randomBytes);
-        return base64Encoder.encodeToString(randomBytes);
     }
 
 }
